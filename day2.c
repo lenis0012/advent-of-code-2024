60d6a039f57af78f6f74dfaaf4cdd43fc897e308
Lennart ten Wolde <lenny.devmail@gmail.com>
2024-12-03 18:30:05 +0100

    chore: solve d2

--- day2.c
+++ day2.c
@@ -6,6 +6,86 @@
 
 #include "aoc.h"
 
+struct Array {
+    int *values;
+    unsigned int length;
+};
+
+struct Array parse_line(char *line) {
+    struct Array array = {
+        .values = calloc(1024, sizeof(int)),
+        .length = 0
+    };
+
+    char *next = line;
+    while (true) {
+        line = next;
+        int value = strtod(line, &next);
+        if (line == next) {
+            break;
+        }
+
+        array.values[array.length++] = value;
+    }
+
+    return array;
+}
+
+struct Array copy_levels_without(struct Array original, int index) {
+    struct Array array = {
+        .values = calloc(1024, sizeof(int)),
+        .length = original.length - 1
+    };
+
+    memcpy(array.values, original.values, index * sizeof(int));
+    memcpy(&array.values[index], &original.values[index + 1], (original.length - index - 1) * sizeof(int));
+    return array;
+}
+
+int is_increasing(struct Array levels) {
+    int last = levels.values[0];
+
+    for (int i = 1; i < levels.length; i++) {
+        if (last >= levels.values[i]) {
+            return i;
+        }
+        last = levels.values[i];
+    }
+
+    return 0;
+}
+
+int is_decreasing(struct Array levels) {
+    int last = levels.values[0];
+
+    for (int i = 1; i < levels.length; i++) {
+        if (last <= levels.values[i]) {
+            return i;
+        }
+        last = levels.values[i];
+    }
+
+    return 0;
+}
+
+int is_similar(struct Array levels) {
+    int last = levels.values[0];
+
+    for (int i = 1; i < levels.length; i++) {
+        int distance = abs(last - levels.values[i]);
+        if (distance == 0 || distance > 3) {
+            return i;
+        }
+        last = levels.values[i];
+    }
+
+    return 0;
+}
+
+bool levels_valid(struct Array levels) {
+    return is_similar(levels) == 0 && (is_increasing(levels) == 0 || is_decreasing(levels) == 0);
+}
+
 void run_day2() {
     FILE *f = fopen("input.txt", "r");
     char *input = aoc_read_file(f);
@@ -13,36 +93,26 @@ void run_day2() {
     int result1 = 0, result2 = 0;
     input = strtok(input, "\n");
     while (input != NULL) {
-        // plusmid's hybrid parser - parse data and compare immediately
-        bool increasing = true, decreasing = true;
-        int failures = 0;
+        struct Array levels = parse_line(input);
 
-        int lastValue = strtod(input, &input); // Start with first value
-        char *next = input;
-        while (true) {
-            input = next;
-            int value = strtod(input, &next);
-            if (input == next) {
-                break;
-            }
+        bool valid = levels_valid(levels);
+        result1 += valid ? 1 : 0;
 
-            // Check if always increasing or decreasing
-            if (increasing && value <= lastValue) increasing = false;
-            if (decreasing && value >= lastValue) decreasing = false;
-
-            // If ordered and not too different
-            if ((!increasing && !decreasing) || (abs(value - lastValue) > 3)) {
-                failures++;
-            }
-
-            // Update & go to next
-            lastValue = value;
+        if (
+            valid
+            || levels_valid(copy_levels_without(levels, is_increasing(levels)))
+            || levels_valid(copy_levels_without(levels, is_decreasing(levels)))
+            || levels_valid(copy_levels_without(levels, is_similar(levels)))
+        ) {
+            result2 += 1;
         }
 
         // go to next line
+        free(levels.values);
         input = strtok(nullptr, "\n");
     }
-    printf("Result: %d\n", result);
+    printf("Result 1: %d\n", result1);
+    printf("Result 2: %d\n", result2);
 
     fclose(f);
 }
