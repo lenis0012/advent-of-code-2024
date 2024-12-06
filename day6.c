a8c13e28401036159aa54cd3770389d8311e8123
Lennart ten Wolde <lenny.devmail@gmail.com>
2024-12-06 22:06:25 +0100

    feat: day 6

--- day6.c
+++ day6.c
@@ -18,7 +18,15 @@ typedef enum {
 } Direction;
 
 typedef struct {
+    bool hn;
+    bool he;
+    bool hs;
+    bool hw;
+} Visited;
+
+typedef struct {
     char **tiles;
+    Visited* visited;
     int height;
     int width;
 } Map;
@@ -44,6 +52,7 @@ Map *load_map(FILE *f) {
     map->tiles = lines.elements;
     map->height = lines.size;
     map->width = strlen(map->tiles[0]);
+    map->visited = calloc(map->width * map->height, sizeof(Visited));
     return map;
 }
 
@@ -60,15 +69,24 @@ Position find_guard(Map *map) {
     exit(1);
 }
 
-void run_day6() {
-    FILE *f = fopen("input.txt", "r");
-    Map *map = load_map(f);
+bool cycles_at(Map *map, Position guard, Direction direction) {
+    // printf("Checking for cycles at %d, %d\n", guard.x, guard.y);
+    const Position paradox = guard;
+    const char t = map->tiles[paradox.y][paradox.x];
+    map->tiles[paradox.y][paradox.x] = '#';
+    memset(map->visited, 0, map->width * map->height * sizeof(Visited)); // Reset visited state
 
-    Position guard = find_guard(map);
-    Direction direction = NORTH;
+    // while (map->tiles[guard.y][guard.x] == '#') {
+    //     // Change direction and go back
+    //     switch (direction) {
+    //     case NORTH: direction = EAST; break;
+    //     case EAST: direction = SOUTH; break;
+    //     case SOUTH: direction = WEST; break;
+    //     case WEST: direction = NORTH; break;
+    //     }
+    // }
 
-    int result = 0;
-    // While within bounds
+    // do our check
     while (guard.x >= 0 && guard.x < map->width && guard.y >= 0 && guard.y < map->height) {
         // If obstructed
         if (map->tiles[guard.y][guard.x] == '#') {
@@ -81,10 +99,20 @@ void run_day6() {
             }
         }
 
-        if (map->tiles[guard.y][guard.x] != 'X') {
-            // Mark position as visited
-            map->tiles[guard.y][guard.x] = 'X';
-            result++;
+        Visited *visited = &map->visited[guard.y * map->width + guard.x];
+        if (
+            (direction == NORTH && visited->hn)
+            || (direction == EAST && visited->he)
+            || (direction == SOUTH && visited->hs)
+            || (direction == WEST && visited->hw)
+        ) {
+            map->tiles[paradox.y][paradox.x] = t;
+            return true;
+        } else {
+            if (direction == NORTH) visited->hn = true;
+            if (direction == EAST) visited->he = true;
+            if (direction == SOUTH) visited->hs = true;
+            if (direction == WEST) visited->hw = true;
         }
 
         // Move forward
@@ -95,11 +123,72 @@ void run_day6() {
         case WEST: guard.x--; break;
         }
     }
-    printf("Result: %d\n", result);
+
+    map->tiles[paradox.y][paradox.x] = t;
+    return false;
+}
+
+void run_day6() {
+    FILE *f = fopen("input.txt", "r");
+    Map *map = load_map(f);
+
+    Position guard = find_guard(map);
+    Direction direction = NORTH;
+
+    int result = 1;
+    int cycles = 0;
+    // While within bounds
+    while (guard.x >= 0 && guard.x < map->width && guard.y >= 0 && guard.y < map->height) {
+        // If obstructed
+        if (map->tiles[guard.y][guard.x] == '#') {
+            // Change direction and go back
+            switch (direction) {
+            case NORTH: direction = EAST; guard.y++; break;
+            case EAST: direction = SOUTH; guard.x--; break;
+            case SOUTH: direction = WEST; guard.y--; break;
+            case WEST: direction = NORTH; guard.x++; break;
+            }
+        } else if (map->tiles[guard.y][guard.x] == '.') {
+            if (cycles_at(map, guard, direction)) {
+                cycles++;
+                if (map->tiles[guard.y][guard.x] == '.') {
+                    result++;
+                }
+                map->tiles[guard.y][guard.x] = 'O';
+            }
+        }
+
+        if (map->tiles[guard.y][guard.x] == '.') {
+            // Mark position as visited
+            if (direction == NORTH || direction == SOUTH) {
+                map->tiles[guard.y][guard.x] = '|';
+            } else {
+                map->tiles[guard.y][guard.x] = '-';
+            }
+            result++;
+        } else if (
+            (map->tiles[guard.y][guard.x] == '|' && (direction == EAST || direction == WEST))
+            || (map->tiles[guard.y][guard.x] == '-' && (direction == NORTH || direction == SOUTH))
+        ) {
+            map->tiles[guard.y][guard.x] = '+';
+        }
+
+        // Move forward
+        switch (direction) {
+        case NORTH: guard.y--; break;
+        case EAST: guard.x++; break;
+        case SOUTH: guard.y++; break;
+        case WEST: guard.x--; break;
+        }
+    }
+    printf("Result 1: %d\n", result);
+    printf("Result 2: %d\n", cycles);
 
     for (int y = 0; y < map->height; y++) {
-        printf("%s\n", map->tiles[y]);
+        free(map->tiles[y]);
     }
-
+    free(map->tiles);
+    free(map->visited);
+    free(map);
     fclose(f);
 }
--- main.c
+++ main.c
@@ -13,6 +13,33 @@ extern void run_day4();
 extern void run_day5();
 extern void run_day6();
 
+struct object {
+    enum type {
+        tnumber,
+        tfloat,
+        tstring
+    } type;
+    union {
+        float fvalue;
+        int ivalue;
+        char* strvalue;
+    };
+};
+
+int add(struct object* a, struct object *b) {
+    if (a->type == tnumber && b->type == tnumber) {
+        return a->ivalue + b->ivalue;
+    } else if (a->type == tfloat && b->type == tfloat) {
+        return a->fvalue + b->fvalue;
+    } else if (a->type == tfloat && b->type == tnumber) {
+        return a->fvalue + b->ivalue;
+    }else if (a->type == tnumber && b->type == tfloat) {
+        return a->ivalue + b->fvalue;
+    } else {
+        exit(1);
+    }
+}
+
 int main(int argc, char **argv) {
     int day = strtod(argv[1], nullptr);
     switch (day) {
